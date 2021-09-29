ae189c8436515be975be93eecf2d108a6025c310
Mildja <veeraya47@gmail.com>
2021-09-29 23:58:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,10 @@ if(isset($_GET['controller'])&&isset($GET['action']))
     <head></head>
     <body>
         $controller = pages,$action = home
-        <br>[<a href="/school">Home</a>]<br>        
+        <br>[<a href="http://158.108.207.4/db21/">Home</a>]
+        [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=home">Quotation</a>]
+        <br>
+
         <p>Welcome to our homepage เว็บเวร</p>
     </body>
 </html>
