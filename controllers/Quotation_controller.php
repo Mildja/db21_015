063e0990df85b5b82f49831da130df8812d42a0d
njpor <nnppkk.j@gmail.com>
2021-09-29 23:31:57 +0700

    Update Quotation_controller.php

--- controllers/Quotation_controller.php
+++ controllers/Quotation_controller.php
@@ -1,1 +1,9 @@
+<?php
+class QuotationController
+{    public function index_Quotation()
+    {    require_once('views/Quotation/index_Quotation.php');    }
+   // public function error() 
+   // {    require_once("views/pages/error.php");    }
 
+}
+?>
