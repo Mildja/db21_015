2fc043d69c8f2d1ee1d853848bb9e1344d424ff1
njpor <nnppkk.j@gmail.com>
2021-10-02 01:46:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,19 +9,28 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
     $action = 'home';
 }?>
 <html>
-    <head></head>
+    <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mitr&display=swap" rel="stylesheet">
+    <style>
+        body {
+        font-family: 'Mitr', sans-serif;
+        font-size: 20px;
+        }
+
+    </style>
+    </head>
     <body>
         <div Align ="Center">
-            <h1 style="font-family:verdana;">Premium Shop</h1>
-
-        
+            <h1>Premium Shop</h1>
             <br>[<a href="http://158.108.207.4/db21">หนีไป</a>]
             [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=Home">Home</a>]
             [<a href="?controller=Quotation&action=index_Quotation">Quotation</a>]
             [<a href="?controller=QuotationDetails&action=index_QuotationDetails">QuotationDetails</a>]
             <br>
-        <?php require_once("./routes.php");?>
-</div>
+            <?php require_once("./routes.php");?>
+        </div>
        
     </body>
 </html>
