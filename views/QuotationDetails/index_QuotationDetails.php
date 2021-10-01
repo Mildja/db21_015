d58581622b28c56072fc696f4cb09eaf434907a4
Mildja <veeraya47@gmail.com>
2021-10-01 17:59:52 +0700

    Update index_QuotationDetails.php

--- views/QuotationDetails/index_QuotationDetails.php
+++ views/QuotationDetails/index_QuotationDetails.php
@@ -1,22 +1,23 @@
 <table border = 1>
 
-<tr>
-<td>ID</td>
-<td>Q_ID</td>
-<td>P_ID</td>
-<td>Color_ID</td>
-<td>NumOfProduct</td>
-<td>NumColorScreen</td>
-</tr>
+        <tr>
+                <td>ID</td>
+                <td>Q_ID</td>
+                <td>P_ID</td>
+                <td>Color_ID</td>
+                <td>NumOfProduct</td>
+                <td>NumColorScreen</td>
+          
+        </tr>
 <?php foreach($QuotationDetails_list as $QuotationDetails)
 {
         echo "<tr>
-        <td>$QuotationDetails->ID</td>
-        <td>$QuotationDetails->Q_ID</td>
-        <td>$QuotationDetails->P_ID</td>
-        <td>$QuotationDetails->Color_ID</td>
-        <td>$QuotationDetails->NumOfProduct</td>
-        <td>$QuotationDetails->NumColorScreen</td>
+                <td>$QuotationDetails->ID</td>
+                <td>$QuotationDetails->Q_ID</td>
+                <td>$QuotationDetails->P_ID</td>
+                <td>$QuotationDetails->Color_ID</td>
+                <td>$QuotationDetails->NumOfProduct</td>
+                <td>$QuotationDetails->NumColorScreen</td>
         </tr>";
 }
 echo "</table>";
