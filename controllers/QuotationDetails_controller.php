97b9efb91b9aab0f9e196a701a55ba1f76cbd5fc
Mildja <veeraya47@gmail.com>
2021-10-01 17:11:32 +0700

    Update QuotationDetails_controller.php

--- controllers/QuotationDetails_controller.php
+++ controllers/QuotationDetails_controller.php
@@ -2,6 +2,7 @@
 {
         public function index_QuotationDetails()
         {
+           echo "fgdgdg";
             $QuotationDetails_list = QuotationDetails::getAll();
             require_once("./views/QuotationDetails/index_QuotationDetails.php");
         }
