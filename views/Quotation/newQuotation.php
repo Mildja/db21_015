9b1604319dbd602820ef7af84d2b58a070c70cc3
njpor <nnppkk.j@gmail.com>
2021-10-02 04:32:55 +0700

    Update newQuotation.php

--- views/Quotation/newQuotation.php
+++ views/Quotation/newQuotation.php
@@ -1,16 +1,18 @@
 <form method = "get" action = "">
-    <label>ID<input type="text" name="Q_ID"/></label>
-    <label>Date<input type="date" name="Q_date"/></label>
-    <label>Emp_ID<input type="text" name="Emp_ID"/></label>
+    <label>ID<input type="text" name="Q_ID"/></label><br>
+    <label>Date<input type="date" name="Q_date" value="<?php echo date("Y-m-d");?>"/></label>
+    <label>Emp_ID<select name="Emp_ID">
     <?php foreach($Employee_List as $Emp)
     {
         echo "<option value=$Emp->Emp_ID>$Emp->Emp_ID</option>";
     }?>
-    <label>Cus_ID<input type="text" name="Cus_ID"/></label>
+    </label></label><br>
+    <label>Cus_ID<select name="Cus_ID">
     <?php foreach($Customer_List as $Cus)
     {
         echo "<option value=$Cus->Cus_ID>$Cus->Cus_ID</option>";
     }?>
+    </label></label><br>
     <label>Payment<input type="text" name="Q_payment"/></label>
 
     <input type="hidden" name="controller" value="Quotation"/>
