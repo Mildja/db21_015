2e4ae6f15a0b2e14d75375310c3e4b4b126d5f8d
njpor <nnppkk.j@gmail.com>
2021-10-02 03:55:18 +0700

    Update newQuotation.php

--- views/Quotation/newQuotation.php
+++ views/Quotation/newQuotation.php
@@ -2,13 +2,16 @@
     <label>ID<input type="text" name="Q_ID"/></label>
     <label>Date<input type="date" name="Q_date"/></label>
     <label>Emp_ID<input type="text" name="Emp_ID"/></label>
+    <?php foreach($Employee_List as $Emp)
+    {
+        echo "<option value=$Emp->empID>$Eus->Emp_name</option>";
+    }?>
     <label>Cus_ID<input type="text" name="Cus_ID"/></label>
-    <label>Payment<input type="text" name="Q_payment"/></label>
-
     <?php foreach($Customer_List as $Cus)
     {
-        echo "<option value=$Cus->ID>$Cus->Cus_name</option>";
+        echo "<option value=$Cus->cusID>$Cus->Cus_name</option>";
     }?>
+    <label>Payment<input type="text" name="Q_payment"/></label>
 
     <input type="hidden" name="controller" value="Quotation"/>
     <button type="submit" name="action" value="index_Quotation">Back</button>
