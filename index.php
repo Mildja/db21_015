cd90d03f1a51e06dfd1a762bc6967f8656678c94
Mildja <veeraya47@gmail.com>
2021-10-01 16:32:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,7 @@ if(isset($_GET['controller'])&&isset($GET['action']))
 <html>
     <head></head>
     <body>
-<<<<<<< HEAD
-
-=======
-       
->>>>>>> 3b630f1c685f6014d3f2e83d27c692aa2f10b992
+        
         <br>[<a href="http://158.108.207.4/db21">หนีไป</a>]
         [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=home">Home</a>]
         [<a href="?controller=Quotation&action=index_Quotation">Quotation</a>]
