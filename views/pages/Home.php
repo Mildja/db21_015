8a7486486f6421d9b7ea7e26788e577d042b1d80
njpor <nnppkk.j@gmail.com>
2021-10-02 01:47:00 +0700

    Update Home.php

--- views/pages/Home.php
+++ views/pages/Home.php
@@ -1,6 +1,14 @@
-<div Align ="Center">
-<p>Welcome to our homepage เว็บเวร</p>
+<html>
+<style>
 
-<img src="https://pbs.twimg.com/media/DxfjiSJVAAA7HQv.jpg" width="350" height="500" >
+</style>
+
+    <div Align ="Center">
+
+    <p>Welcome to our homepage อิอิ</p>
+
+    <img src="https://pbs.twimg.com/media/DxfjiSJVAAA7HQv.jpg" width="350" height="500" >
 
 </div>
+</html>
+
