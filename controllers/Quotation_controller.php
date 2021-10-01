7ec6afdffa5c5c4c5410097ea868674a183f4cb1
njpor <nnppkk.j@gmail.com>
2021-10-01 15:33:23 +0700

    Update Quotation_controller.php

--- controllers/Quotation_controller.php
+++ controllers/Quotation_controller.php
@@ -3,6 +3,7 @@ class QuotationController
 {
     public function index_Quotation()
     {
+        $Quotation_List = Quotation::getAll();
         require_once('views/Quotation/index_Quotation.php';)
     }
 }?>
