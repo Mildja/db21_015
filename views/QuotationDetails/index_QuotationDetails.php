214272cf0020ad44a5b9355874e96ed6b63777e4
Mildja <veeraya47@gmail.com>
2021-10-01 15:46:31 +0700

    Update index_QuotationDetails.php

--- views/QuotationDetails/index_QuotationDetails.php
+++ views/QuotationDetails/index_QuotationDetails.php
@@ -1,1 +1,23 @@
+<table border = 1>
 
+<tr>
+<td>PL_ID</td>
+<td>Q_ID</td>
+<td>P_ID</td>
+<td>Color_ID</td>
+<td>NumOfProduct</td>
+<td>NumColorScreen</td>
+</tr>
+<?php foreach($QuotationDetails_list as $QuotationDetails)
+{
+        echo "<tr>
+        <td>$QuotationDetails->PL_ID</td>
+        <td>$QuotationDetails->Q_ID</td>
+        <td>$QuotationDetails->P_ID</td>
+        <td>$QuotationDetails->Color_ID</td>
+        <td>$QuotationDetails->NumOfProduct</td>
+        <td>$QuotationDetails->NumColorScreen</td>
+        </tr>";
+}
+echo "</table>";
+?>
