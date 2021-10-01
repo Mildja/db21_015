13087275071b95f2e48bca0f87d6b338a8e16258
njpor <nnppkk.j@gmail.com>
2021-10-02 04:32:58 +0700

    Update index_Quotation.php

--- views/Quotation/index_Quotation.php
+++ views/Quotation/index_Quotation.php
@@ -26,8 +26,9 @@ tr:hover{background-color:#f5f5f5}
         <button type="submit" name="action" value="search">Search</button>
     </form>
 
-    Add Quotation <a href="?controller=index_Quotation&action=newQuotation">Click</a>
-
+   
+    Add Quotation <a href="?controller=Quotation&action=newQuotation">Click</a>
+    
 
 <br>
 <table style="width:80%">
