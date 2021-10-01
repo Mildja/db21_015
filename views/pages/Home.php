2053cec22de0ff7bb383d9d27a7f171b490c24e3
njpor <nnppkk.j@gmail.com>
2021-10-01 23:38:40 +0700

    Update Home.php

--- views/pages/Home.php
+++ views/pages/Home.php
@@ -1,1 +1,4 @@
 <p>Welcome to our homepage เว็บเวร</p>
+
+<img src="https://pbs.twimg.com/media/DxfjiSJVAAA7HQv.jpg" /><br>
+<img src="https://s.isanook.com/ho/0/rp/rc/w850h510/yacxacm1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hvLzAvdWQvMzUvMTc1NTUzLzYxMzIwMy5qcGc=.jpg" />
