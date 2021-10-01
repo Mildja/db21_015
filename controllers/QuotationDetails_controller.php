c4a8fc9ed0525fd914d867ee65a7bb213616d845
Mildja <veeraya47@gmail.com>
2021-10-01 15:46:17 +0700

    Update QuotationDetails_controller.php

--- controllers/QuotationDetails_controller.php
+++ controllers/QuotationDetails_controller.php
@@ -1,1 +1,10 @@
+<?php class QuotationDetails_controller
+{
+        public function index()
+        {
+            $QuotationDetails_list = QuotationDetails::getAll();
+            require_once("views/QuotationDetails/index_QuotationDetails");
+        }
 
+
+}?>
