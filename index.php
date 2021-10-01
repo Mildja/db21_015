38dcc4ad20b2b3908f90b2db40ac3727136e2ef0
njpor <nnppkk.j@gmail.com>
2021-10-01 16:58:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ if(isset($_GET['controller'])&&isset($GET['action']))
     <head></head>
     <body>
         <h1 style="font-family:verdana;">Premium Shop</h1>
-       
+        <?php echo "controller= ".$controller.",action=".$action;?>
         <br>[<a href="http://158.108.207.4/db21">หนีไป</a>]
         [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=home">Home</a>]
         [<a href="?controller=Quotation&action=index_Quotation">Quotation</a>]
         [<a href="?controller=QuotationDetails&action=index_QuotationDetails">QuotationDetails</a>]
         <br>
-
+        <?php require_once("./routes.php");?>
       
         <p>Welcome to our homepage เว็บเวร</p>
     </body>
