416ed22750e607a03f4f1dc838ef5cf0ba53146e
njpor <nnppkk.j@gmail.com>
2021-10-02 00:22:01 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,7 +1,7 @@
 <?php
 class PagesController
-{    public function home()
-    {    require_once('views/pages/home.php');    }
+{    public function Home()
+    {    require_once('views/pages/Home.php');    }
     public function error() 
     {    require_once('views/pages/error.php');    }
 
