70bed4e063f4fced728dbfa633a45b3877eae379
Mildja <veeraya47@gmail.com>
2021-10-01 15:51:53 +0700

    Update index_Quotation.php

--- views/Quotation/index_Quotation.php
+++ views/Quotation/index_Quotation.php
@@ -1,1 +1,27 @@
+<table border = 1>
+    <tr>
+        <td>ID</td>
+        <td>Date</td>
+        <td>Emp_ID</td>
+        <td>Cus_ID</td>
+        <td>Q_payment</td>
+        <td>Q_percent</td>
+    </tr>
+
+
+<?php 
+foreach($Quotation_List as $Quotation)
+{
+    echo "<tr>
+        <td>$Quotation->ID</td>
+        <td>$Quotation->date</td>
+        <td>$Quotation->empid</td>
+        <td>$Quotation->cusid</td>
+        <td>$Quotation->payment</td>
+        <td>$Quotation->percent</td>
+
+    </tr>";
+}
+echo "</table>";
+?>
 
