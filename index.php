6c88d3519f72f74aca2e4149628411c52f510e65
Mildja <veeraya47@gmail.com>
2021-10-01 16:38:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,13 @@ if(isset($_GET['controller'])&&isset($GET['action']))
 <html>
     <head></head>
     <body>
-        
+
         <br>[<a href="http://158.108.207.4/db21">หนีไป</a>]
         [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=home">Home</a>]
         [<a href="?controller=Quotation&action=index_Quotation">Quotation</a>]
         [<a href="?controller=QuotationDetails&action=index_QuotationDetails">QuotationDetails</a>]
         <br>
+        <?php require_once("routes.php") ?>
         <p>Welcome to our homepage เว็บเวร</p>
     </body>
 </html>
