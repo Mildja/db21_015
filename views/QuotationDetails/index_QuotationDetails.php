9cd3695a27eead64d677008a751d1fab276bdf7e
Mildja <veeraya47@gmail.com>
2021-10-02 01:06:29 +0700

    Update index_QuotationDetails.php

--- views/QuotationDetails/index_QuotationDetails.php
+++ views/QuotationDetails/index_QuotationDetails.php
@@ -1,7 +1,30 @@
-<div Align ="Center">
-        <table border = 1>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+tr:hover{background-color:#f5f5f5}
+</style>
+</head>
+<body>
+
+
+
+        <table style="width:80%">
         สร้างรายละเอียดใบสั่งซื้อ <a href=?controller=QuotationDetails&action=NewQuotationDetails> click</a><br>
-        <tr>
+        <br>
+        
+        <tr bgcolor = "pink">
                 <td>ID</td>
                 <td>Q_ID</td>
                 <td>P_ID</td>
