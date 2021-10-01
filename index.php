b9574cf2e1d64497b165b695726373d4787ca2aa
njpor <nnppkk.j@gmail.com>
2021-10-02 00:21:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,16 +11,17 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
 <html>
     <head></head>
     <body>
-        <h1 style="font-family:verdana;">Premium Shop</h1>
-       
-        <br>[<a href="http://158.108.207.4/db21">หนีไป</a>]
-        [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=home">Home</a>]
-        [<a href="?controller=Quotation&action=index_Quotation">Quotation</a>]
-        [<a href="?controller=QuotationDetails&action=index_QuotationDetails">QuotationDetails</a>]
-        <br>
-        <?php require_once("./routes.php");?>
+        <div Align ="Center">
+            <h1 style="font-family:verdana;">Premium Shop</h1>
 
-        <p>'w'</p>
+        
+            <br>[<a href="http://158.108.207.4/db21">หนีไป</a>]
+            [<a href="http://158.108.207.4/db21/db21_015/?controller=pages&action=Home">Home</a>]
+            [<a href="?controller=Quotation&action=index_Quotation">Quotation</a>]
+            [<a href="?controller=QuotationDetails&action=index_QuotationDetails">QuotationDetails</a>]
+            <br>
+        <?php require_once("./routes.php");?>
+</div>
        
     </body>
 </html>
