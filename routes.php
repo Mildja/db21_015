7c48231bc4cd90a065da4996d1b1c47848159be8
njpor <nnppkk.j@gmail.com>
2021-10-02 23:42:46 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-$controllers = array('pages'=>['Home', 'error'],'Quotation'=>['index_Quotation','newQuotation','addQuotation','search','update']
+$controllers = array('pages'=>['Home', 'error'],'Quotation'=>['index_Quotation','newQuotation','addQuotation','search','updateForm','update']
                 ,'QuotationDetails'=>['index_QuotationDetails','NewQuotationDetails','addQuotationDetails','updateFrom']); //list controller
 
 
