e7dd871273f6582f2fb37274e9c5a6ef6b67933f
njpor <nnppkk.j@gmail.com>
2021-10-03 02:02:04 +0700

    Update updateForm.php

--- views/Quotation/updateForm.php
+++ views/Quotation/updateForm.php
@@ -6,16 +6,16 @@
 <form method = "GET" action = "">
 
     <label>เลขใบสั่งซื้อ (ID)  <input type="text" name="ID"
-            value="<?php echo $Quotation->ID; ?>"/></label><br>
+        value="<?php echo $Quotation->ID; ?>"/></label><br>
 
     <label>วันที่ทำการสั่งซื้อ (Date)  <input type="date" name="date" 
-            value="<?php echo $Quotation->date;?>/></label><br>
+        value="<?php echo date("Y-m-d",strtotime($Quotation->date));?>"/></label><br>
     
     <label>รหัสพนักงาน (Emp_ID)  <select name="empID">
     <?php foreach($Employee_List as $E)
     {
         echo "<option value=$E->ID";
-        if($E->ID==$Quotation->empID){echo "selected='selected'";}
+        if($E->ID=$Quotation->empID){echo "selected='selected'";}
         echo ">$E->ID</option>";
     }?>
     </select></label><br>
@@ -24,16 +24,16 @@
     <?php foreach($Customer_List as $C)
     {
         echo "<option value=$C->ID";
-        if($C->ID==$Quotation->cusID){echo "selected='selected'";}
+        if($C->ID=$Quotation->cusID){echo "selected='selected'";}
         echo ">$C->ID</option>";
     }?>
     </select></label><br>
 
     <label>รูปแบบการชำระเงิน (มัดจำ/เครดิต)  <input type="text" name="payment"
-            value="<?php echo $Quotation->payment;?>"/></label><br>
+        value="<?php echo $Quotation->payment;?>"/></label><br>
     
     <label>เปอร์เซ็นต์มัดจำ (Percent)  <input type="number" name="percent"
-            value="<?php echo $Quotation->percent;?>"/></label><br>
+        value="<?php echo $Quotation->percent;?>"/></label><br>
 
     <input type="hidden" name="controller" value="Quotation"/><br>
     <button type="submit" name="action" value="index_Quotation">Back</button>
