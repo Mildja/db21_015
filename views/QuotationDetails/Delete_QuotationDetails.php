c80432473a5c2b47aae56c43c2702e90285ab942
Mildja <veeraya47@gmail.com>
2021-10-03 03:53:39 +0700

    Update Delete_QuotationDetails.php

--- views/QuotationDetails/Delete_QuotationDetails.php
+++ views/QuotationDetails/Delete_QuotationDetails.php
@@ -1,1 +1,10 @@
+<?php echo "<br>คุณต้องการที่จะลบรายละเอียดใบสั่งซื้อหรือไม่<br>
+        <br>$QuotationDetails->ID
+        $QuotationDetails->P_ID $QuotationDetails->Color_ID br>";?>
 
+<form method="get" action="">
+    <input type="hidden" name="controller" value="QuotationDetails"/>
+    <input type="hidden" name="ID" value="<?php echo $QuotationDetails->ID;?>"/>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="Delete">Delete</button>
+</form>
