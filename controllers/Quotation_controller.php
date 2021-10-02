903eb2b5428ab1f3335fbecc67b8b0fd6b8d185a
njpor <nnppkk.j@gmail.com>
2021-10-03 00:00:00 +0700

    Update Quotation_controller.php

--- controllers/Quotation_controller.php
+++ controllers/Quotation_controller.php
@@ -24,9 +24,7 @@ class QuotationController
             $cusID = $_GET['cusID'];
             $payment = $_GET['payment'];
             $percent = $_GET['percent'];
-           
             Quotation::add($ID,$date,$empID,$cusID,$payment,$percent);
-
             QuotationController::index_Quotation();
     }
 
@@ -35,5 +33,26 @@ class QuotationController
         $key=$_GET['key'];
         $Quotation_List = Quotation::search($key);
         require_once("./views/Quotation/index_Quotation.php");
+    }
+
+    public function updateForm()
+    {
+        $ID=$_GET['Q_ID'];
+        $Quotation = Quotation::get($ID);
+        $Customer_List = Quotation::getAll();
+        $Employee_List = Quotation::getAll();
+        require_once('./views/Quotation/updateForm.php');
+    }
+
+    public function update()
+    {
+            $ID = $_GET['ID'];
+            $date = $_GET['date'];
+            $empID = $_GET['empID'];
+            $cusID = $_GET['cusID'];
+            $payment = $_GET['payment'];
+            $percent = $_GET['percent'];
+            Quotation::update($ID,$date,$empID,$cusID,$payment,$percent);
+            QuotationController::index_Quotation();
     }
 }?>
