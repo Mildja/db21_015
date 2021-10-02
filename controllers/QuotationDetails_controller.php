620b8e4c075815c5d54a0eb9faf7598e7222804e
Mildja <veeraya47@gmail.com>
2021-10-02 18:10:51 +0700

    Update QuotationDetails_controller.php

--- controllers/QuotationDetails_controller.php
+++ controllers/QuotationDetails_controller.php
@@ -5,6 +5,14 @@
             $QuotationDetails_list = QuotationDetails::getAll();
             require_once("./views/QuotationDetails/index_QuotationDetails.php");
         }
+
+        public function newQuotation()
+        {
+            $Quotation_List = Quotation::getAll();
+            $Customer_List = Customer::getAll();
+            $Employee_List = Employee::getAll();
+            require_once("./views/Quotation/newQuotation.php");
+        }
         /*public function update()
         {
                 $ID=$_GET['QD_ID'];
