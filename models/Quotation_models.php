9f8b799fc5b33ed1158f909733ded5e865082de8
njpor <nnppkk.j@gmail.com>
2021-10-02 21:17:08 +0700

    Update Quotation_models.php

--- models/Quotation_models.php
+++ models/Quotation_models.php
@@ -79,11 +79,11 @@ class Quotation{
     public static function add($ID,$date,$empID,$cusID,$payment,$percent)
     {
         require("connection_connect.php");
-        $sql = "INSERT into Quotation(Q_ID,Q_date,Emp_ID,Cus_ID,Q_payment,Q_percent) values ('$ID','$date','$empID','$cusID','$payment')";
+        $sql = "INSERT into Quotation(Q_ID,Q_date,Emp_ID,Cus_ID,Q_payment,Q_percent) values ('$ID','$date','$empID','$cusID','$payment',$percent')";
         $result = $conn->query($sql);
         require("connection_close.php");
 
-        return "add success $result rows";
+        return 5555 ;
     }
 
     public static function update($ID,$date,$empID,$cusID,$payment)
