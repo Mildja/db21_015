1c860a53d62ba41a274b7cb424fa5ffafcbe64ce
njpor <nnppkk.j@gmail.com>
2021-10-03 02:02:02 +0700

    Update index_Quotation.php

--- views/Quotation/index_Quotation.php
+++ views/Quotation/index_Quotation.php
@@ -53,7 +53,7 @@ foreach($Quotation_List as $Quotation)
         <td>$Quotation->cusID</td>
         <td>$Quotation->payment</td>
         <td>$Quotation->percent</td>
-        <td><a href=?controller=Quotation&action=update&Q_ID=$Quotation->ID>Update</a></td>
+        <td><a href=?controller=Quotation&action=updateForm&Q_ID=$Quotation->ID>Update</a></td>
         <td><a href=?controller=Quotation&action=delete&Q_ID=$Quotation->ID>Delete</a></td>
 
     </tr>";
