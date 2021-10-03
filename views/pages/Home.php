dc0845fc64cd85b44a488c973bdde55526b16236
njpor <nnppkk.j@gmail.com>
2021-10-04 02:55:42 +0700

    Update Home.php

--- views/pages/Home.php
+++ views/pages/Home.php
@@ -1,13 +1,25 @@
 <html>
 <style>
-
+   
 </style>
 
     <div Align ="Center">
 
-    <p>Welcome to our homepage อิอิ</p>
+    
 
-    <img src="https://pbs.twimg.com/media/DxfjiSJVAAA7HQv.jpg" width="350" height="500" >
+    <body>
+
+        <p>Welcome to our homepage อิอิ</p>
+        <img src="https://pbs.twimg.com/media/DxfjiSJVAAA7HQv.jpg" width="350" height="500" ><br><br>
+        <font size = 3>วีรยา ศรีรุจิเมธากร 6220504780 / นภัส เจียมประเสริฐ 6220502132</font>
+
+        <br><br><br><br><br><br>
+    </body>
+    
+    
+    
+
+    
 
 </div>
 </html>
