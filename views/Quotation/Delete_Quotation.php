eecb92f9a61920b89af08438e1bd9e86f83e948d
njpor <nnppkk.j@gmail.com>
2021-10-03 19:57:47 +0700

    Update Delete_Quotation.php

--- views/Quotation/Delete_Quotation.php
+++ views/Quotation/Delete_Quotation.php
@@ -1,1 +1,17 @@
+<?php echo "<br>Are u sure to DELETE this Quotation (?)<br>
+        <br>
+        $Quotation->ID
+        $Quotation->date 
+        $Quotation->empID 
+        $Quotation->cusID
+        $Quotation->payment
+        $Quotation->percent
+        <br>";
+?>
 
+<form method="get" action="">
+    <input type="hidden" name="controller" value="Quotation"/>
+    <input type="hidden" name="Q_ID" value="<?php echo $Quotation->ID;?>"/>
+    <button type="submit" name="action" value="index_Quotation">Back</button>
+    <button type="submit" name="action" value="delete">Delete</button>
+</form>
